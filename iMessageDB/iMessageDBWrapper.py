00d6af3c014f3013c3ae449629570bbe6fa05396
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2019-08-29 20:17:04 -0300

    remove repetition from random messages method conditionals

--- iMessageDB/iMessageDBWrapper.py
+++ iMessageDB/iMessageDBWrapper.py
@@ -51,28 +51,19 @@ class iMessageDBWrapper(object):
 
     def get_messages_random(self, amount, handle_id=None, chat_id=None, clean_data=True):
 
-        if handle_id and chat_id:
-            where_clause = "handle_id == %s AND chat_id == %s" % (handle_id, chat_id)
-
+        if not (handle_id or chat_id):
             df = pandas.read_sql_query("SELECT * FROM message\
-                WHERE %s AND ROWID IN (SELECT ROWID FROM message\
-                    WHERE %s ORDER BY RANDOM() LIMIT %i)" % (where_clause, where_clause, amount), self.conn)
-
-        elif handle_id:
-            where_clause = "handle_id == %s" % (handle_id)
-
-            df = pandas.read_sql_query("SELECT * FROM message\
-                WHERE %s AND ROWID IN (SELECT ROWID FROM message\
-                    WHERE %s ORDER BY RANDOM() LIMIT %i)" % (where_clause, where_clause, amount), self.conn)
-
-        elif chat_id:
-            where_clause = "chat_id == %s" % (chat_id)
-
-            df = pandas.read_sql_query("SELECT * FROM message\
-                WHERE %s AND ROWID IN (SELECT ROWID FROM message\
-                    WHERE %s ORDER BY RANDOM() LIMIT %i)" % (where_clause, where_clause, amount), self.conn)
+                        WHERE ROWID IN (SELECT ROWID FROM message ORDER BY RANDOM() LIMIT %i)" % (amount, self.conn))
         else:
+            if handle_id and chat_id:
+                where_clause = "handle_id == %s AND chat_id == %s" % (handle_id, chat_id)
+            elif handle_id:
+                where_clause = "handle_id == %s" % (handle_id)
+            elif chat_id:
+                where_clause = "chat_id == %s" % (chat_id)
+
             df = pandas.read_sql_query("SELECT * FROM message\
-                WHERE ROWID IN (SELECT ROWID FROM message ORDER BY RANDOM() LIMIT %i)" % (amount), self.conn)
+                    WHERE %s AND ROWID IN (SELECT ROWID FROM message\
+                        WHERE %s ORDER BY RANDOM() LIMIT %i)" % (where_clause, where_clause, amount), self.conn)
 
         return clean_message_df(df) if clean_data else df
